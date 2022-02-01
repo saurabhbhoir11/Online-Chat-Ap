987cf1d577a893dda448862845bf7a24789ad752
protocorn <chordiasahil24@gmail.com>
2022-02-01 17:33:18 +0530

    database

--- app/src/main/java/com/example/onlinechatapp/Login.java
+++ app/src/main/java/com/example/onlinechatapp/Login.java
@@ -2,13 +2,39 @@ package com.example.onlinechatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.onlinechatapp.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+    ActivityLoginBinding binding;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding=ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        auth=FirebaseAuth.getInstance();
+
+        auth.signInWithEmailAndPassword(binding.email.getText().toString,binding.pass.getText().toString).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                if(auth.getCurrentUser().isEmailVerified()){
+                    startActivity(new Intent(Login.this,MainActivity.class));
+                }
+                else{
+                    Toast.makeText(Login.this, "Please Verify Your Email", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/onlinechatapp/Register.java
+++ app/src/main/java/com/example/onlinechatapp/Register.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.onlinechatapp.databinding.ActivityRegisterBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Register extends AppCompatActivity {
+    ActivityRegisterBinding binding;
+    FirebaseAuth auth;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding=ActivityRegisterBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(binding.getRoot());
+        auth=FirebaseAuth.getInstance();
+        firestore=FirebaseFirestore.getInstance();
+
+
+        auth.createUserWithEmailAndPassword(binding.email.getText().toString(),binding.pass.getText().toString());
+
+
     }
 }
