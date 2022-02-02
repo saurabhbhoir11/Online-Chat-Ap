470f670b95b1ef29e573280182218a5986d527bd
saurabhbhoir11 <91054017+saurabhbhoir11@users.noreply.github.com>
2022-02-02 19:23:19 +0530

    register activity changes

--- app/src/main/java/com/example/onlinechatapp/Login.java
+++ app/src/main/java/com/example/onlinechatapp/Login.java
@@ -1,16 +1,12 @@
 package com.example.onlinechatapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.onlinechatapp.databinding.ActivityLoginBinding;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
--- app/src/main/java/com/example/onlinechatapp/Register.java
+++ app/src/main/java/com/example/onlinechatapp/Register.java
@@ -1,14 +1,15 @@
 package com.example.onlinechatapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.onlinechatapp.databinding.ActivityRegisterBinding;
+import com.example.onlinechatapp.models.Users;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -33,19 +34,35 @@ public class Register extends AppCompatActivity {
         binding.registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                auth.createUserWithEmailAndPassword(binding.email.getText().toString(), binding.pass.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            user.sendEmailVerification();
-                            Toast.makeText(Register.this, "Verification Link sent to "+binding.email.getText() , Toast.LENGTH_SHORT).show();
-                            user = auth.getCurrentUser();
+                if(binding.password.getText().toString() != binding.confPass.getText().toString()){
+                    binding.confPass.setError("The Passwords does not match");
+                    return;
+                }
+                else if (binding.password.getText().length() < 6 ){
+                    binding.password.setError("The Password must be at least 6 characters");
+                    return;
+                }
+                else if (binding.email.getText().toString().isEmpty()) {
+                    binding.email.setError("Please enter an email");
+                    return;
+                }
+                else {
+                    auth.createUserWithEmailAndPassword(binding.email.getText().toString(), binding.password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                user.sendEmailVerification();
+                                Toast.makeText(Register.this, "Verification Link sent to " + binding.email.getText(), Toast.LENGTH_SHORT).show();
+                                user = auth.getCurrentUser();
+                                Users users = new Users(binding.email.getText().toString(), binding.password.getText().toString());
+                                users.setUserid(user.getUid());
+                                firestore.collection("Users").document(user.getUid()).set(users);
+                                startActivity(new Intent(Register.this, MainActivity.class));
 
-                            startActivity(new Intent(Register.this, MainActivity.class));
-
+                            }
                         }
-                    }
-                });
+                    });
+                }
             }
         });
 
