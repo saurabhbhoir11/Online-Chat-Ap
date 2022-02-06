b426e397e4e8bf1666ffdd9d76e18f6b037edf91
protocorn <chordiasahil24@gmail.com>
2022-02-06 13:22:05 +0530

    profile pic

--- app/src/main/java/com/example/onlinechatapp/Fragments/ProfileFrag.java
+++ app/src/main/java/com/example/onlinechatapp/Fragments/ProfileFrag.java
@@ -1,16 +1,39 @@
 package com.example.onlinechatapp.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.onlinechatapp.R;
+import com.example.onlinechatapp.databinding.FragmentProfileBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 public class ProfileFrag extends Fragment {
+    FragmentProfileBinding binding;
+    FirebaseStorage firebaseStorage;
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
 
 
     public ProfileFrag() {
@@ -20,7 +43,74 @@ public class ProfileFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding=FragmentProfileBinding.inflate(inflater,container,false);
+        firebaseStorage=FirebaseStorage.getInstance();
+        firestore=FirebaseFirestore.getInstance();
+        auth=FirebaseAuth.getInstance();
+
+        firestore.collection("Users").document(auth.getCurrentUser().getUid()).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                Glide.with(getContext()).load(value.get("profilepic")).centerCrop().placeholder(R.drawable.user).into(binding.profileImage);
+                binding.profileName.setText(""+value.get("username"));
+
+            }
+        });
+
+        binding.profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 33);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==33) {
+            if (data.getData() != null) {
+                Uri sfile = data.getData();
+                binding.profileImage.setImageURI(sfile);
+
+                final StorageReference reference = firebaseStorage.getReference("profilepic")
+                        .child(auth.getCurrentUser().getUid());
+
+                reference.putFile(sfile).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                HashMap<String, Object> hashMap = new HashMap<>();
+                                hashMap.put("profilepic", uri.toString());
+
+                                firestore.collection("Users").document(auth.getCurrentUser().getUid()).update(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(getContext(), "Profile Picture Set Successfully", Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getContext(), "Something Went Wrong, Please Try Again", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        });
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
     }
 }
