6bd0a743fa27145990193ecaf14a9fa452ac3c6f
saurabhbhoir11 <91054017+saurabhbhoir11@users.noreply.github.com>
2022-02-09 18:06:56 +0530

    serach activity  layout

--- app/src/main/java/com/example/onlinechatapp/SearchAcitvity.java
+++ app/src/main/java/com/example/onlinechatapp/SearchAcitvity.java
@@ -1,13 +1,26 @@
 package com.example.onlinechatapp;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.onlinechatapp.Adapter.SearchAdapter;
 import com.example.onlinechatapp.databinding.ActivitySearchAcitvityBinding;
 import com.example.onlinechatapp.models.Users;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 
 import java.util.ArrayList;
 
@@ -28,5 +41,62 @@ public class SearchAcitvity extends AppCompatActivity {
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(SearchAcitvity.this);
         binding.availableusers.setLayoutManager(layoutManager);
+        SearchAdapter adapter = new SearchAdapter(list,SearchAcitvity.this);
+        binding.availableusers.setAdapter(adapter);
+        binding.searchname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchUsers(s.toString());
+            }
+
+            private void searchUsers(String s){
+                FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                Query query =FirebaseFirestore.getInstance().collection("Users").orderBy("username").startAt(s).endAt(s+"\uf8ff");
+                query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        list.clear();
+                        for (DocumentSnapshot value1 : value.getDocuments()){
+                            Users users = value1.get("username", Users.class);
+
+                            assert users !=null;
+                            assert firebaseUser !=null;
+
+                            if(!users.getUserid().equals(firebaseUser.getUid())){
+                                list.add(users);
+                            }
+
+                        }
+
+                    }
+                });
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        });
+
+        mUserDatabase.collection("Users").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                list.clear();
+                for (DocumentSnapshot value2 : value.getDocuments()){
+                    Users users = value2.get("username",Users.class);
+                    users.setUserid(value2.getData());
+                    if(!users.getUserid().equals(FirebaseAuth.getInstance().getUid())){
+                        list.add(users);
+                    }
+                }
+            }
+        });
     }
+
 }
