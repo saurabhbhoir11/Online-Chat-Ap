83498edc8373ecdba5def3a98d1045ae6cd6656c
saurabhbhoir11 <91054017+saurabhbhoir11@users.noreply.github.com>
2022-02-13 12:05:38 +0530

    chat detail Activity

---
+++ app/src/main/java/com/example/onlinechatapp/ChatDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.onlinechatapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_detail);
+    }
+}
