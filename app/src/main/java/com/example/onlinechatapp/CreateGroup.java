fb40ee180bbc302bfab8ab6bfeca31c1f1767cdd
saurabhbhoir11 <91054017+saurabhbhoir11@users.noreply.github.com>
2022-04-02 16:27:42 +0530

    group activity

---
+++ app/src/main/java/com/example/onlinechatapp/CreateGroup.java
@@ -0,0 +1,14 @@
+package com.example.onlinechatapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+}
